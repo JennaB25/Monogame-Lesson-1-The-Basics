491a939e680a90cf473debef139125f238d9b9b5
362700239 <jennarbloor@gmail.com>
2023-04-04 11:01:38 -0400

    Add project files.

---
+++ Monogame Lesson 1 - The Basics/Game1.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using SharpDX.Direct3D9;
+
+namespace Monogame_Lesson_1___The_Basics
+{
+    public class Game1 : Game
+    {
+        Texture2D dinoTexture;
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.PreferredBackBufferHeight = 500; 
+            _graphics.ApplyChanges(); 
+            this.Window.Title = "First Monogame Test";
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            dinoTexture = Content.Load<Texture2D>("dino");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Bisque);
+
+            _spriteBatch.Begin();
+            //dinoTexture = Vector2(50, 50);
+            for (int i = 0; i < 5; i++)             
+            {             
+                _spriteBatch.Draw(dinoTexture, new Vector2(i, i), Color.White);      
+            }
+            
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
